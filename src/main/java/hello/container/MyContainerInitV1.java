a65bcb61cf4993bc580a5870358da3bf708cd221
chocolaggibbiddori <didrkd684@naver.com>
2023-03-10 19:49:37 +0900

    Init ServletContainer V1

---
+++ src/main/java/hello/container/MyContainerInitV1.java
@@ -0,0 +1,16 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+
+import java.util.Set;
+
+public class MyContainerInitV1 implements ServletContainerInitializer {
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) {
+        System.out.println("MyContainerInitV1.onStartup");
+        System.out.println("MyContainerInitV1 c = " + c);
+        System.out.println("MyContainerInitV1 ctx = " + ctx);
+    }
+}
